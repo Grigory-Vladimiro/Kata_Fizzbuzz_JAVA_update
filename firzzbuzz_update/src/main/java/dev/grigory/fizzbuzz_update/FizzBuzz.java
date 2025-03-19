bd355ccc1360c61040fd629d4c785867ffd54a68
Grigory Vladimiro <grigori.vladimiro@gmail.com>
2025-03-19 21:03:38 +0100

    method_implementation_to_pass_basic_tests

--- firzzbuzz_update/src/main/java/dev/grigory/fizzbuzz_update/FizzBuzz.java
+++ firzzbuzz_update/src/main/java/dev/grigory/fizzbuzz_update/FizzBuzz.java
@@ -4,7 +4,12 @@ import java.util.List;
 public class FizzBuzz {
 
         public String evaluate(int number) {
-            return "";
+            boolean isFizz = number % 3 == 0 || String.valueOf(number).contains("3");
+            boolean isBuzz = number % 5 == 0 || String.valueOf(number).contains("5");
+            if (isFizz && isBuzz) return "FizzBuzz";
+            if (isFizz) return "Fizz";
+            if (isBuzz) return "Buzz";
+            return String.valueOf(number);
         }
 
         public List<String> generate(int start, int end) {
