c29bbb518ca8059f8df886d84f4c1936540c73f8
Grigory Vladimiro <grigori.vladimiro@gmail.com>
2025-03-19 20:55:31 +0100

    temp_empty_class

---
+++ firzzbuzz_update/src/main/java/dev/grigory/fizzbuzz_update/FizzBuzz.java
@@ -0,0 +1,13 @@
+package dev.grigory.fizzbuzz_update;
+
+import java.util.List;
+public class FizzBuzz {
+
+        public String evaluate(int number) {
+            return "";
+        }
+
+        public List<String> generate(int start, int end) {
+            return List.of();
+        }
+}
